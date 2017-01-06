4857909be51ee35020884fcd6bbd2d328a3bc7cb
perfectDevs2017 <youssef.elam91@hotmail.com>
2017-01-06 15:52:42 +0000

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -28,19 +28,44 @@ function getSequence(className,callback) {
 
 Parse.Cloud.beforeSave("Article", function (request, response) { 
 
-    if (request.object.isNew()) {
-      var className = "Article";
-        getSequence(className,function(sequence) { 
-            if (sequence) {
-                request.object.set("bindingByte", sequence);
-                response.success();
-            } else {
-                response.error('Could not get a sequence.')
-            }
+    var groupId = request.object.get("groupId");
+    var name = request.object.get("name");
+    var quantity = request.object.get("quantity");
+    //Do not re-declare the class
+    var query = new Parse.Query("Article");
+    //Check for existing Article
+    query.equalTo("groupId", groupId);
+
+    query.first().then(function (object) {
+
+      if (object) {
+        //Article exists and needs updating.
+        // Do not save the object attached to the request.
+        //Instead, update existing object.
+        object.set('name', name);
+        object.set('quantity', quantity);
+        object.save().then(function () {
+          response.error('Error updated existing Article');
+        },
+        function (error) {
+          response.error("Error: " + error.code + " " + error.message);
         });
-    } else {
-        response.success();
-    }
+      } else {
+        //Continuing and save the new Article object because it is not a duplicate.
+        var className = "Article";
+            getSequence(className,function(sequence) { 
+                if (sequence) {
+                    request.object.set("bindingByte", sequence);
+                    response.success();
+                } else {
+                    response.error('Could not get a sequence.');
+                }
+            });
+      }
+    },
+    function (error) {
+      response.error("Error: " + error.code + " " + error.message);
+});
 
 });
 
