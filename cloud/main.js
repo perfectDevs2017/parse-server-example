41cf09b213027fb41b49815690e9a971f93a8595
perfectDevs2017 <youssef.elam91@hotmail.com>
2017-01-09 15:55:50 +0000

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -30,41 +30,47 @@ Parse.Cloud.beforeSave("Article", function (request, response) {
 
 	var Entity = Parse.Object.extend("Article");
     var query = new Parse.Query(Entity);
-	var name = request.object.get("name");
-	var quantity = request.object.get("quantity");
-	query.equalTo("groupId","1002");
-    query.first({ 
-        success: function(object) {
-		    if(object)
-		    {
-				request.object.set('objectId', object.objectId);
-				/*
-				var name2 = object.get('name');
-				if(name != name2)
-				{
-					request.object.set('name', "aaa");
-					request.object.save();
-					response.success();
-					object.save();
-				}
-				*/
+    // Add query filters to check for uniqueness
+    query.equalTo("groupId", "1002");
+    query.first().then(function(existingObject) {
+      if (existingObject) {
+        // Update existing object. here you can do all the object updates you want 
+        //if (request.object.get("columnToUpdate") != undefined){
+          existingObject.set('name','JWT');
+        //}
+        return existingObject.save();
+        response.error();
+      } else {
+        // Pass a flag that this is not an existing object
+        //return Parse.Promise.as(false);
+		var className = "Article";
+		getSequence(className,function(sequence) { 
+			if (sequence) {
+				request.object.set("bindingByte", sequence);
+				response.success();
+			} else {
+				response.error('Could not get a sequence.');
 			}
-			else
-			{
-				var className = "Article";
-				getSequence(className,function(sequence) { 
-					if (sequence) {
-						request.object.set("bindingByte", sequence);
-						response.success();
-					} else {
-						response.error('Could not get a sequence.');
-					}
-				});
+		});
+      }
+    }).then(function(existingObject) {
+      if (existingObject) {
+        // Existing object, stop initial save
+        response.error("Existing object");
+      } else {
+        // New object, let the save go through
+        var className = "Article";
+		getSequence(className,function(sequence) { 
+			if (sequence) {
+				request.object.set("bindingByte", sequence);
+				response.success();
+			} else {
+				response.error('Could not get a sequence.');
 			}
-			
-        }, error: function (error) {
-            console.log(error);
-        }
+		});
+      }
+    }, function (error) {
+      response.error(error);
     });
   
 });
