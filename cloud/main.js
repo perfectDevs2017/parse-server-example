aa95da90043158ec19dd44525739d10a5ace1099
perfectDevs2017 <youssef.elam91@hotmail.com>
2017-01-10 10:24:25 +0000

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -28,7 +28,7 @@ function getSequence(className,callback) {
 
 Parse.Cloud.beforeSave("Article", function (request, response) { 
 
-var Entity = Parse.Object.extend("Article");
+    var Entity = Parse.Object.extend("Article");
     var query = new Parse.Query(Entity);
 	query.equalTo("groupId",request.object.get("groupId"));
     query.first({ 
@@ -37,6 +37,8 @@ var Entity = Parse.Object.extend("Article");
 		    {
 				if(request.object.get("name") !== object.get("name"))
 				{
+					object.set("objectId",object.objectId);
+					request.object.set("objectId", object.objectId);
 					object.set('name', request.object.get("name"));
 					//object.save();
 					response.success();
